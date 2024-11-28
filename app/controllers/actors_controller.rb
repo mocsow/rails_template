4e3ff975e7b290fbfea9a1b62c5b5bb20bb5a56b
mocsow <mcsow@alumni.chicagobooth.edu>
2024-11-28 11:30:53 -0600

    Initial commit

---
+++ app/controllers/actors_controller.rb
@@ -0,0 +1,61 @@
+class ActorsController < ApplicationController
+  def create
+    puts "Parameters: #{params.inspect}" # This line will print all parameters to the log
+    
+    #Create a new director with parameters from the form
+    a = Actor.new
+    a.name = params.fetch("the_name")
+    a.dob = params.fetch("the_dob")
+    a.bio = params.fetch("the_bio")
+    a.image = params.fetch("the_image")
+    a.save
+
+    #Redirect to the movies index page after saving
+    redirect_to("/actors")
+  end
+
+  def destroy
+    the_id = params.fetch("an_id")
+
+    matching_records = Actor.where({:id => the_id})
+
+    the_actor = matching_records.at(0)
+
+    the_actor.destroy
+
+    redirect_to("/actors")
+  end
+
+  def update
+    a_id = params.fetch("the_id")
+
+    matching_records = Actor.where({:id => a_id})
+    the_actor = matching_records.at(0)
+
+    the_actor.name = params.fetch("the_name")
+    the_actor.dob = params.fetch("the_dob")
+    the_actor.bio = params.fetch("the_bio")
+    the_actor.image = params.fetch("the_image")
+
+    the_actor.save
+
+    redirect_to("/actors/#{the_actor.id}")
+
+  end
+
+  def index
+    matching_actors = Actor.all
+    @list_of_actors = matching_actors.order({ :created_at => :desc })
+
+    render({ :template => "actor_templates/index" })
+  end
+
+  def show
+    the_id = params.fetch("path_id")
+
+    matching_actors = Actor.where({ :id => the_id })
+    @the_actor = matching_actors.at(0)
+      
+    render({ :template => "actor_templates/show" })
+  end
+end
